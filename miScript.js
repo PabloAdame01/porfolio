2b317668cf2ee9137c862e71b06973cd8fe9745a
PabloAdame01 <estudiopablo52@gmail.com>
2025-06-15 12:46:33 +0200

    V0.5

--- miScript.js
+++ miScript.js
@@ -1,9 +1,20 @@
-$(document).ready(function() {
-  $('#toggleDark').on('click', function() {
+$(document).ready(function () {
+  // Al cargar la página, aplicar el modo oscuro si está guardado
+  if (localStorage.getItem('darkMode') === 'true') {
+    $('html').addClass('dark');
+  } else {
+    $('html').removeClass('dark');
+  }
+
+  // Al hacer clic en el botón, alternar y guardar en localStorage
+  $('#toggleDark').on('click', function () {
     $('html').toggleClass('dark');
+    let darkModeActive = $('html').hasClass('dark');
+    localStorage.setItem('darkMode', darkModeActive ? 'true' : 'false');
   });
 
+  // Configuración de Tailwind
   tailwind.config = {
-            darkMode: 'class',
-        }
+    darkMode: 'class',
+  };
 });
